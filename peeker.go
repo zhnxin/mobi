88ea8d369d19f19938269b01c24e56008d2b068d
Peter Birk Pakkenberg <peter.birk.nielsen+github@gmail.com>
2018-07-28 20:26:52 +0100

    Documented peeker to get rid of linter errors

--- peeker.go
+++ peeker.go
@@ -1,19 +1,24 @@
 package mobi
 
-type Peeker []uint8
+// Peeker is a multimorphic byte slice
+type Peeker []byte
 
+// Magic as mobiMagicType (string enum)
 func (p Peeker) Magic() mobiMagicType {
 	return mobiMagicType(p)
 }
 
+// String as plain old string
 func (p Peeker) String() string {
 	return string(p)
 }
 
-func (p Peeker) Bytes() []uint8 {
+// Bytes as raw bytes
+func (p Peeker) Bytes() []byte {
 	return p
 }
 
+// Len as len of slice
 func (p Peeker) Len() int {
 	return len(p)
 }
